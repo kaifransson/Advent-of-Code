9da506e12f2c861ff58247cb71525decbfd79f60
kaifransson <franssonkai@gmail.com>
2017-12-13 23:29:22 +0100

    solve part 1 of day 11

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,9 +1,15 @@
 module Day11 where
 
+import           Control.Arrow
 import           Data.Char
 import           Data.List.Split
+import           Data.Monoid
 import           Paths_AoC
 import           Util
+
+parseDirections :: String -> [Direction]
+parseDirections input = let textDirs = fmap (fmap toUpper) . splitOn "," $ input
+                         in fmap read textDirs
 
 data Direction = N
                | NE
@@ -13,18 +19,30 @@ data Direction = N
                | NW
                deriving (Show, Read, Eq)
 
-parseDirections :: String -> [Direction]
-parseDirections input = let textDirs = fmap (fmap toUpper) . splitOn "," $ input
-                         in fmap read textDirs
+phi :: Direction -> (Int, Int)
+phi NE = (1, 0)
+phi NW = (0, 1)
+phi SE = (0, -1)
+phi SW = (-1, 0)
+phi N  = (1, 1)
+phi S  = (-1, -1)
 
-simplify :: [Direction] -> [Direction]
-simplify [] = []
+simplify :: [Direction] -> (Int, Int)
+simplify dirs = let coordinates = fmap phi dirs
+                 in (getSum *** getSum) . mconcat . fmap (Sum *** Sum) $ coordinates
 
 distance :: [Direction] -> Int
-distance = length . simplify
+distance dirs = let (x, y)    = simplify dirs
+                    absCoords = (abs x, abs y)
+                 in reduce absCoords
+              where reduce (x, y)
+                        | x > 0 && y > 0 = 1 + reduce (x-1, y-1)
+                        | x > 0 = 1 + reduce (x-1, y)
+                        | y > 0 = 1 + reduce (x, y-1)
+                        | otherwise = 0
 
 day11 :: IO ()
 day11 = do
     input <- getInputForDay 11
     let directions = parseDirections input
-    print directions
+    print $ distance directions
