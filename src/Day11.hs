2e44878f4b4d8db96de3cf45f77e9ba707bd27b3
kaifransson <franssonkai@gmail.com>
2017-12-13 21:07:14 +0100

    add direction data structure with parsing function

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,4 +1,24 @@
 module Day11 where
 
+import           Data.Char
+import           Data.List.Split
 import           Paths_AoC
+import           Util
 
+data Direction = N
+               | NE
+               | SE
+               | S
+               | SW
+               | NW
+               deriving (Show, Read, Eq)
+
+parseDirections :: String -> [Direction]
+parseDirections input = let textDirs = fmap (fmap toUpper) . splitOn "," $ input
+                         in fmap read textDirs
+
+day11 :: IO ()
+day11 = do
+    input <- getInputForDay 11
+    let directions = parseDirections input
+    print directions
