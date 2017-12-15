94dea727a6cec39b8aa700a63796c04ee03868b6
kaifransson <franssonkai@gmail.com>
2017-12-15 13:05:57 +0100

    add instruction data type

--- src/Day8.hs
+++ src/Day8.hs
@@ -2,6 +2,30 @@ module Day8 where
 
 import           Util
 
+type Registry = String
+
+data Action = Increment
+            | Decrement
+            deriving (Show, Read, Eq)
+
+data Condition = Cond { reg :: Registry
+                      , op  :: CompareOp
+                      , arg :: Int }
+                      deriving (Show)
+
+data CompareOp = Eq
+               | Leq
+               | Lt
+               | Geq
+               | Gt
+               deriving (Show, Read, Eq)
+
+data Instruction = Inst { target :: Registry
+                        , action :: Action
+                        , amount :: Int
+                        , cond   :: Condition }
+                        deriving (Show)
+
 day8 :: IO ()
 day8 = do
     input <- getInputForDay 8
