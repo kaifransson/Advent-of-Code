66f4108c71e28dbe28b7ba251ea1872e7cad4f7e
kaifransson <franssonkai@gmail.com>
2017-12-18 16:28:21 +0100

    add some parsers

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,5 +1,8 @@
 module Day8 where
 
+import           Text.Parsec
+import           Text.Parsec.String
+import           Text.Parsec.Token
 import           Util
 
 type Registry = String
@@ -25,6 +28,41 @@ data Instruction = Inst { target :: Registry
                         , amount :: Int
                         , cond   :: Condition }
                         deriving (Show)
+
+word :: Parser String
+word = many $ noneOf " "
+
+pRegistry :: Parser Registry
+pRegistry = word
+
+pAction :: Parser Action
+pAction = try $
+    const Increment <$> string "inc"
+    <|>
+    const Decrement <$> string "dec"
+
+pOperation :: Parser CompareOp
+pOperation = do
+    undefined
+
+pCond :: Parser Condition
+pCond = do
+    string "if"
+    registry <- pRegistry
+    operation <- pOperation
+    argument <- undefined
+    return $ Cond registry operation argument
+
+pInstruction :: Parser Instruction
+pInstruction = do
+    tar <- pRegistry
+    space
+    act <- pAction
+    space
+    am <- undefined
+    space
+    cond <- pCond
+    return $ Inst tar act am undefined
 
 day8 :: IO ()
 day8 = do
